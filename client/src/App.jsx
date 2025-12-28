1bf11bc9f216ba43419d503b04fcb28de417d6b0
Armaan Jhanji <armaanj32@gmail.com>
2025-12-28 23:05:56 +0400

    feat: add pages to app and introduce themeprovider + cssbasline

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { muiTheme } from "./theme";
+
+import { Layout } from "./components/Layout";
+import { Landing } from "./pages/Landing";
+import { NotFound } from "./pages/NotFound";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <ThemeProvider theme={muiTheme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
 
-export default App
+            <Route path='/' element={<Landing />}/>
+            <Route path="*" element={<NotFound />} />
+
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+
+    </ThemeProvider>
+  );
+};
+
+export default App;
