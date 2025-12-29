d9ca1eb5b9e518528935c8747b502e39d110ff6d
Armaan Jhanji <armaanj32@gmail.com>
2025-12-30 00:45:19 +0400

    fix: reset products to array

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -75,7 +75,7 @@ export function Filters({ isLoading, onChange }) {
 
         setExchangeType("");
         setInterval("");
-        setProducts("");
+        setProducts([]);
         setAiesecer("");
     };
 
