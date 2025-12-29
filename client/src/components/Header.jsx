7e1363bb9d407df91b29b1315a5f7b8550271a15
Armaan Jhanji <armaanj32@gmail.com>
2025-12-30 00:15:15 +0400

    style: update header margins

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -19,18 +19,18 @@ export function Header({ title, subtitle }) {
     const theme = useTheme();
     
     return (
-        <Box sx={{ maxWidth: 1200, mx: "auto", display: "flex", flexDirection: "column", mb: 2, px: 0.5 }}>
+        <Box sx={{ maxWidth: 1200, mx: "auto", display: "flex", flexDirection: "column", mb: 4, px: 0.5 }}>
             <Box>
                 <Typography
                     variant="h2"
                     color={theme.palette.text.primary}
+                    mb={0.5}
                 >
                     {title}
                 </Typography>
                 <Typography
                     variant="h5"
                     color={theme.palette.text.secondary}
-                    mb={2}
                 >
                     {subtitle}
                 </Typography>
